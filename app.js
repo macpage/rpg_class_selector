5c31717352f7fbca62d5cc82d7af0b7fcce262a6
Mac <sophanpa@gmail.com>
2023-05-14 21:56:02 +0200

    Add basic character editor and character preview

--- app.js
+++ app.js
@@ -16,12 +16,19 @@ function create_user(username, rpg_class) {
   this.rpg_class = rpg_class;
 }
 
+// imgs
+// Hair
+const hair_img = document.createElement('img');
+const hair_styles = [];
+hair_styles[0] = 'pics/pixil-layer-No hair.png';
+hair_styles[1] = 'pics/hair_style_1.png';
 const user = new create_user('Mac', rpg_classes[0]);
 console.log(rpg_classes[0].name);
 // View
 const body = document.querySelector('body');
 const menu = document.createElement('div');
 const create_box = document.createElement('div');
+const title = document.createElement('p');
 
 const username = document.createElement('input');
 const username_btn = document.createElement('button');
@@ -47,6 +54,9 @@ function create_name_input() {
   username_btn.setAttribute('id', 'username_btn');
   username_btn.innerHTML = 'OK';
   username.placeholder = 'Enter your Name...';
+  title.innerHTML = 'Welcome to RPG Character Creator';
+  title.setAttribute('id', 'title');
+  create_box.append(title);
   create_box.append(username);
   create_box.append(username_btn);
   menu.append(create_box);
@@ -57,10 +67,11 @@ function class_select() {
   create_box.style.justifyContent = 'start';
   create_box.classList.add('stretchMore');
   create_box.style.width = '50vw';
-  create_box.style.height = '60vh';
+  create_box.style.height = '65vh';
   user.username = username.value;
   username.remove();
   username_btn.remove();
+  title.remove();
 
   // Add new scene
   box_background.classList.add('box_background');
@@ -141,6 +152,10 @@ function class_select() {
     td_5.style.fontSize = '18px';
     td_7.style.fontWeight = 'bold';
     td_7.style.fontSize = '18px';
+
+    e.addEventListener('click', () => {
+      set_up_creator();
+    });
   });
 
   setTimeout(() => {
@@ -149,6 +164,124 @@ function class_select() {
       console.log('ooohh');
     });
   }, 1500);
+}
+
+// Character Creator
+const char_creator = document.createElement('div');
+char_creator.setAttribute('id', 'char_creator');
+const selectors = document.createElement('div');
+selectors.setAttribute('id', 'selectors');
+const char_preview = document.createElement('div');
+char_preview.setAttribute('id', 'char_preview');
+
+function set_up_creator() {
+  box_background.remove();
+  create_box.append(char_creator);
+  char_creator.append(selectors);
+  char_creator.append(char_preview);
+  create_box.classList.add('stretchMore2');
+  create_box.style.width = '60vw';
+  create_box.style.height = '75vh';
+
+  // Selectors
+  // Hair
+  const hair_selector = document.createElement('div');
+  const hair_selector_name = document.createElement('p');
+  const hair_selector_back_btn = document.createElement('button');
+  const hair_selector_num = document.createElement('p');
+  const hair_selector_forward_btn = document.createElement('button');
+
+  hair_selector.setAttribute('id', 'hair_selector');
+  hair_selector.setAttribute('class', 'selector');
+
+  hair_selector_name.innerHTML = 'Hair';
+  let hair_num = 0;
+  hair_selector_num.innerHTML = hair_num;
+  selectors.append(hair_selector);
+  hair_selector.append(hair_selector_name);
+  hair_selector.append(hair_selector_back_btn);
+  hair_selector.append(hair_selector_num);
+  hair_selector.append(hair_selector_forward_btn);
+
+  function hair_change_style() {
+    char_preview.append(hair_img);
+    hair_img.src = hair_styles[hair_num];
+    hair_selector_back_btn.addEventListener('click', () => {
+      hair_num--;
+      hair_selector_num.innerHTML = hair_num;
+      hair_img.src = hair_styles[hair_num];
+    });
+    hair_selector_forward_btn.addEventListener('click', () => {
+      hair_num++;
+      hair_selector_num.innerHTML = hair_num;
+      hair_img.src = hair_styles[hair_num];
+    });
+  }
+
+  // Hair color
+  function select_color() {
+    const color_selector = document.createElement('div');
+    color_selector.setAttribute('id', 'color_selector');
+    char_creator.append(color_selector);
+
+    const all_colors = [];
+
+    for (let i = 0; i < 6; i++) {
+      console.log(all_colors.length);
+      const e = document.createElement('div');
+      e.style.height = '2vw';
+      e.style.width = '2vw';
+
+      color_selector.append(e);
+    }
+  }
+
+  hair_selector.addEventListener('click', () => {
+    select_color();
+  });
+  // Eyes
+  const eye_selector = document.createElement('div');
+  const eye_selector_name = document.createElement('p');
+  const eye_selector_back_btn = document.createElement('button');
+  const eye_selector_num = document.createElement('p');
+  const eye_selector_forward_btn = document.createElement('button');
+
+  eye_selector.setAttribute('id', 'eye_selector');
+  eye_selector.setAttribute('class', 'selector');
+
+  eye_selector_name.innerHTML = 'Eyes';
+  let eye_num = 0;
+  eye_selector_num.innerHTML = eye_num;
+  selectors.append(eye_selector);
+  eye_selector.append(eye_selector_name);
+  eye_selector.append(eye_selector_back_btn);
+  eye_selector.append(eye_selector_num);
+  eye_selector.append(eye_selector_forward_btn);
+
+  function eye_change_style() {
+    char_preview.append(eye_img);
+    eye_img.src = hair_styles[hair_num];
+    hair_selector_back_btn.addEventListener('click', () => {
+      hair_num--;
+      hair_selector_num.innerHTML = hair_num;
+      hair_img.src = hair_styles[hair_num];
+    });
+    hair_selector_forward_btn.addEventListener('click', () => {
+      hair_num++;
+      hair_selector_num.innerHTML = hair_num;
+      hair_img.src = hair_styles[hair_num];
+    });
+  }
+
+  // Mouth
+
+  // Skin
+
+  // Outfit
+
+  // Char Preview
+
+  hair_change_style();
 }
 
 create_menu();
